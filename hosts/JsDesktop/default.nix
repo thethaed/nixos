4df392785569b57efd066c1769cb179149477ed1
John <github@johnlundin.se>
2023-08-12 18:33:57 +0200

    3rd flake try

--- hosts/JsDesktop/default.nix
+++ hosts/JsDesktop/default.nix
@@ -5,5 +5,12 @@
   [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
+  
+  
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
 
 }
