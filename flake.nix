527cd045caf882ed0d2a5715537fc9982f4fe542
John <github@johnlundin.se>
2023-08-12 18:17:34 +0200

    first flake try

---
+++ flake.nix
@@ -0,0 +1,116 @@
+{
+  description = "Js nixos configs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    # home-manager.url = "github:nix-community/home-manager/release-23.05";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # emacs-overlay.url = "github:nix-community/emacs-overlay/master";
+    
+  };
+
+  outputs = inputs@{ 
+    self, 
+  #  home-manager, 
+    nixpkgs, 
+    unstable, 
+    nixos-hardware, 
+ #   emacs-overlay, 
+    ... }:
+    let
+      system = "x86_64-linux";
+      pkgs =
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      defaultNixOptions = {
+        nix.autoOptimiseStore = true;
+      };
+      mkComputer = configurationNix: userName: extraModules: extraHomeModules: inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit system inputs pkgs nixos-hardware; };
+        modules = [
+          #Secrets management
+         # sops-nix.nixosModules.sops
+          #Machine config
+          configurationNix
+          defaultNixOptions
+          #User config
+ #         (./. + "/users/${userName}")
+          #Home manager
+ #         home-manager.nixosModules.home-manager
+ #         {
+ #           home-manager.useGlobalPkgs = true;
+ #           home-manager.useUserPackages = true;
+ #           home-manager.users."${userName}" = {
+ #             imports = [ (./. + "/users/${userName}/home.nix") ] ++ extraHomeModules;
+ #           };
+ #         }
+        ] ++ extraModules;
+      };
+    in
+    {
+      nixosConfigurations = {
+#        nixosvm = mkComputer
+#          ./machines/nixosvm #machine specific configuration
+#          "dovalperin" #default user
+#          [
+#            ./modules/gnome
+#            ./modules/1password
+#            ./modules/tailscale
+#            ./modules/ssh
+#          ] #modules to load
+#          [
+#            ./modules/zsh
+#          ]; #modules to be loaded by home-manager
+        JsDesktop = mkComputer
+          ./machines/JsDesktop #machine specific configuration
+          "john" #default user
+          [
+            ./modules/xserver
+            ./modules/gnome
+#            ./modules/tailscale
+#            ./modules/ssh
+#            ./modules/zoom
+#            ./modules/browsers
+#            ./modules/postgresql
+#            ./modules/redis
+            #./modules/1password #needs to be a systemPackage to install polkit policy
+          ] #modules to load
+          [
+#            ./modules/zsh
+#            ./modules/emacs
+          ]; #modules to be loaded by home-manager
+#        ascent = mkComputer
+#          ./machines/ascent
+#          "worker"
+#          [
+#            ./modules/ssh
+#            ./modules/tailscale
+#            ./modules/matrix
+#          ]
+#          [ ];
+#        humblegeoffrey = mkComputer
+#          ./machines/humblegeoffrey
+#          "dovalperin"
+#          [
+#            ./modules/xserver
+#            ./modules/gnome
+#            ./modules/ssh
+#            ./modules/browsers
+#            ./modules/postgresql
+#            ./modules/redis
+#            ./modules/tailscale
+#            ./modules/zoom
+#          ] #modules to load
+#          [
+#            ./modules/zsh
+#            ./modules/emacs
+#          ]; #modules to be loaded by home-manager
+      };
+    };
+}
+
+
---
+++ modules/gnome/default.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+{
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # https://github.com/NixOS/nixpkgs/issues/75867
+  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.gnome.seahorse.out}/libexec/seahorse/ssh-askpass";
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
+    gnomeExtensions.advanced-alttab-window-switcher
+    gnomeExtensions.adwaita-theme-switcher
+    gnomeExtensions.gtile
+    gnome.gnome-tweaks
+    wmctrl
+    gnvim
+    adwaita-qt
+    libsForQt5.qtstyleplugins
+  ];
+
+  qt5 = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
+
+  services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
+}
